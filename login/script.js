449c7078d4e8acf4128612f1ef348cb6c9f284bf
hknocal <hknocal@outlook.com>
2023-11-28 14:53:56 +0100

    refactoring of api_url for easy switch between local and production api

--- login/script.js
+++ login/script.js
@@ -1,127 +1,128 @@
-let div = ''  // let: can be changed, but not re-declared. Hoisting behavior is Reference error
-// var: can be changed AND re-declared. Hoisting behavior is 'undefined'
-// const: can not be changed or re-declared. Hoisting behavior is Reference error
-document.addEventListener("DOMContentLoaded", function() { // wait for DOM to load
-document.getElementById('signupForm').addEventListener('submit', signup);
-document.getElementById('loginForm').addEventListener('submit', login);
-document.getElementById('getSecretForm').addEventListener('submit', getSecret)
-document.getElementById('deleteUserForm').addEventListener('submit', deleteUser)
-document.getElementById('logoutForm').addEventListener('submit', logout)
-div = document.getElementById('container')
+// HUSK AT SKIFTE API URL!
+//const API_URL = "http://localhost:8080";
+const API_URL = "https://cosmeticsbackend.azurewebsites.net";
+
+let div = '';
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('signupForm').addEventListener('submit', signup);
+  document.getElementById('loginForm').addEventListener('submit', login);
+  document.getElementById('getSecretForm').addEventListener('submit', getSecret);
+  document.getElementById('deleteUserForm').addEventListener('submit', deleteUser);
+  document.getElementById('logoutForm').addEventListener('submit', logout);
+  div = document.getElementById('container');
 });
 
-function myFetch(endpoint, method, payload=null, token){
-return fetch(`https://cosmeticsbackend.azurewebsites.net/${endpoint}`,  // important to start this line with 'return'
-{
-method: method,
-body: payload,
-headers:{'content-type': 'application/json',
-  'Authorization': 'Bearer ' + token }
-})
-.then(function (res) {
-return res.json(); // wait for .json() to complete
-})
-.then(function (data) { // the data, provided by server response
-return data
-})
+function myFetch(endpoint, method, payload = null, token) {
+  return fetch(`${API_URL}/${endpoint}`, {
+    method: method,
+    body: payload,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    }
+  })
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        return data;
+      });
 }
 
-function getToken(){
-const localstorage_user = JSON.parse(localStorage.getItem('user'))
-return  localstorage_user.token
+function getToken() {
+  const localstorage_user = JSON.parse(localStorage.getItem('user'));
+  return localstorage_user.token;
 }
 
-function printThis(mydiv, txt, color){
-mydiv.insertAdjacentHTML(
-'beforeend',
-`<span style="background-color: ${color}">${txt}</code>`,
-);
+function printThis(mydiv, txt, color) {
+  mydiv.insertAdjacentHTML(
+      'beforeend',
+      `<span style="background-color: ${color}">${txt}</code>`,
+  );
 }
 
 function signup(event) {
-event.preventDefault()
-const nameField = document.getElementById("nameField").value;
-const passwordFieldSignup = document.getElementById("passwordFieldSignup").value;
-let payload = {
-username: nameField,
-password: passwordFieldSignup
-};
-payload = JSON.stringify(payload)
-fetch("https://cosmeticsbackend.azurewebsites.net/signup",
-{
-method: "POST",
-body: payload,
-headers:{'content-type': 'application/json'}
-})
-.then(function (res) {
-return res.json();
-})
-.then(function (data) {
-printThis(div, JSON.stringify(data), "green")
-})
+  event.preventDefault();
+  const nameField = document.getElementById("nameField").value;
+  const passwordFieldSignup = document.getElementById("passwordFieldSignup").value;
+  let payload = {
+    username: nameField,
+    password: passwordFieldSignup
+  };
+  payload = JSON.stringify(payload);
+  fetch(`${API_URL}/signup`, {
+    method: "POST",
+    body: payload,
+    headers: { 'content-type': 'application/json' }
+  })
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        printThis(div, JSON.stringify(data), "green");
+      });
 }
 
-
 function login(event) {
-event.preventDefault()
-const usernameField = document.getElementById("usernameField").value;
-const passwordField = document.getElementById("passwordField").value;
-let payload = {
-username: usernameField,
-password: passwordField
-};
-payload = JSON.stringify(payload)
-fetch("https://cosmeticsbackend.azurewebsites.net/login",
-{
-method: "POST",
-body: payload,
-headers:{'content-type': 'application/json'}
-})
-.then(function (res) {
-return res.json();
-})
-.then(function (res) {
-localStorage.setItem('user', JSON.stringify(res));
-printThis(div,JSON.stringify(res), "green" )
-})
+  event.preventDefault();
+  const usernameField = document.getElementById("usernameField").value;
+  const passwordField = document.getElementById("passwordField").value;
+  let payload = {
+    username: usernameField,
+    password: passwordField
+  };
+  payload = JSON.stringify(payload);
+  fetch(`${API_URL}/login`, {
+    method: "POST",
+    body: payload,
+    headers: { 'content-type': 'application/json' }
+  })
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (res) {
+        localStorage.setItem('user', JSON.stringify(res));
+        printThis(div, JSON.stringify(res), "green");
+      });
 }
 
 function getSecret(event) {
-event.preventDefault()
-if(localStorage.getItem('user') == undefined){
-printThis(div, "No token. Login first", "red")
-return
-}
-const paramField = document.getElementById("paramField").value;
-const payload = {
-paramField: paramField
-};
-myFetch("getSecret", "POST", payload, getToken())
-.then((data) => {
-printThis(div, JSON.stringify(data), "green")
-})
+  event.preventDefault();
+  if (localStorage.getItem('user') == undefined) {
+    printThis(div, "No token. Login first", "red");
+    return;
+  }
+  const paramField = document.getElementById("paramField").value;
+  const payload = {
+    paramField: paramField
+  };
+  myFetch("getSecret", "POST", payload, getToken())
+      .then((data) => {
+        printThis(div, JSON.stringify(data), "green");
+      });
 }
 
 function deleteUser(event) {
-event.preventDefault()
-if(localStorage.getItem('user') == undefined){
-printThis(div, "No token. Login first", "red")
-return
-}
-const usernameDeleteField = document.getElementById("usernameDeleteField").value;
-let payload = {
-username: usernameDeleteField,
-password: "password"  // dummy password here
-};
-payload = JSON.stringify(payload)
-myFetch("deleteUser", "DELETE", payload, getToken())
-.then((data) => {
-printThis(div, JSON.stringify(data), "green")
-})
+  event.preventDefault();
+  if (localStorage.getItem('user') == undefined) {
+    printThis(div, "No token. Login first", "red");
+    return;
+  }
+  const usernameDeleteField = document.getElementById("usernameDeleteField").value;
+  let payload = {
+    username: usernameDeleteField,
+    password: "password"
+  };
+  payload = JSON.stringify(payload);
+  myFetch("deleteUser", "DELETE", payload, getToken())
+      .then((data) => {
+        printThis(div, JSON.stringify(data), "green");
+      });
 }
 
 function logout(event) {
-event.preventDefault()
-localStorage.removeItem('user')
-printThis(div, "You have logged out.", "green")
+  event.preventDefault();
+  localStorage.removeItem('user');
+  printThis(div, "You have logged out.", "green");
 }
