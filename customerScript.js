462bda281784b64fb08a4e7db37de32f382f278c
hknocal <hknocal@outlook.com>
2023-12-15 01:19:28 +0100

    booking adjustments

--- customerScript.js
+++ customerScript.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function () {
-   // const apiUrl = 'http://localhost:8080';
+    //const apiUrl = 'http://localhost:8080';
     const apiUrl = 'https://cosmeticsbackend.azurewebsites.net';
     const logoutBtn = document.getElementById('logoutBtn');
     const userListBody = document.getElementById('customerListBody');
